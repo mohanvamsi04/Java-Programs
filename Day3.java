005f9d10d47eb0f11a3602a91c4ef66f8a1474a4
B MOHAN VAMSI <94058971+mohanvamsi04@users.noreply.github.com>
2023-01-22 18:21:41 +0530

    ASCII values of a character

--- Day3.java
+++ Day3.java
@@ -23,7 +23,7 @@ Output:
 */
 
 import java.util.*;
-class Main {
+class Day3 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter a character");
