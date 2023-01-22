0b73635a76c6fae678b75de1676552d7d68c368f
B MOHAN VAMSI <94058971+mohanvamsi04@users.noreply.github.com>
2023-01-22 18:20:29 +0530

    AlphabetOrNot

--- Day2.java
+++ Day2.java
@@ -24,9 +24,10 @@ Not an alphabet
 */
 
 import java.util.*;
-class Main {
+class Day2 {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
+         System.out.println("Enter a character : ");
         char ch=sc.next().charAt(0);
         if((ch>=65&&ch<=90)||(ch>=97&&ch<=122)){
             System.out.println("Alphabet");
