918986a72c2c3a9a39da9d677f4538ce8f11d9fd
B MOHAN VAMSI <94058971+mohanvamsi04@users.noreply.github.com>
2023-01-22 18:19:10 +0530

    VowelOrConsonant

--- Day1.java
+++ Day1.java
@@ -36,6 +36,7 @@ import java.util.*;
 class Day1 {
     public static void main(String[] args) {
      Scanner sc=new Scanner(System.in);
+     System.out.println("Enter a character : ");
      char ch=sc.next().charAt(0);
      if((ch>='a'&&ch<='z')||(ch>='A'&&ch<='Z')){
      if(ch=='a'||ch=='A'||ch=='e'||ch=='E'||ch=='i'||ch=='I'||ch=='o'||ch=='O'||ch=='u'||ch=='U'){
