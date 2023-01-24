bb4e4471edc1be6630af1dece7575f1e6fa35edd
B MOHAN VAMSI <94058971+mohanvamsi04@users.noreply.github.com>
2023-01-24 23:08:11 +0530

    RootsOfQuadraticEquations

---
+++ Day8.java
@@ -0,0 +1,51 @@
+/*
+Day 8 coding Statement:  Write a program to find roots of a quadratic equation
+
+Description
+
+Get the values of a, b and c (coefficients of quadratic equation) as input from the user and calculate the roots and print as the output.
+
+Input
+
+Enter the value of a, b and c : 1 -6 9
+
+Output
+
+Roots are equal
+
+Root 1= root 2 = 3.00
+*/
+
+import java.util.*;
+public class Day8
+{
+	public static void main(String[] args) {
+	
+	 Scanner sc = new Scanner(System.in);
+	 int a,b,c;
+	 System.out.println("Enter the values of a, b and c (coefficients of quadratic equation) :");
+	 a=sc.nextInt();
+	 b=sc.nextInt();
+	 c=sc.nextInt();
+	 double r1,r2;
+	 double d=b*b-4*a*c;
+	 r1=(-b+Math.sqrt(d))/(2*a);
+	 r2=(-b-Math.sqrt(d))/(2*a);
+	 
+	 if(r1==r2 && d==0){
+	     System.out.println("Roots are real and equal .");
+	     System.out.println("root1 = "+r1+" \nroot2 = "+r2);
+	 }
+	 if(d>0){
+	     System.out.println("Roots are real and distinct");
+	      System.out.println("root1 = "+r1+" \nroot2 = "+r2);
+	 }
+	 
+	 if(d<0){
+	     System.out.println("Roots are complex ");
+	    
+	 }
+	 
+	 
+	}
+}
